02068d1c947fa9d6e212ec91b054f2dbddeb894d
shenghaoNi <1159417019@qq.com>
2022-01-01 22:35:27 +0800

    spring boot 要用java8 不能用14

---
+++ SpringbootHelloworld/src/main/java/com/itheima/controller/com/itheima/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.itheima.controller.com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*   引导类. springboot 项目的入口
+*
+* */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+
+    }
+}
